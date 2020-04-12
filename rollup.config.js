19955352cb1197d4d61157c354b1d2176ca3f0bc
ingvord <tla-ingvord@yandex.ru>
2020-04-12 14:50:27 +0300

    Setup project

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import multi from '@rollup/plugin-multi-entry';
+import pkg from './package.json';
+
+export default [
+    // browser-friendly UMD build
+    {
+        input: 'src/*.js',
+        external: ['rxjs', '@waltz-controls/eventbus'],
+        output: {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: 'inline'
+        },
+        plugins: [
+            resolve(), // so Rollup can find `ms`
+            commonjs(), // so Rollup can convert `ms` to an ES module
+            multi()
+        ]
+    }
+];
